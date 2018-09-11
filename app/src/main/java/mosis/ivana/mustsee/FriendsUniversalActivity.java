e5f4387198970a6c68075b285504cd36e81ed087
ivanamili <32956858+ivanamili@users.noreply.github.com>
2018-09-12 00:30:41 +0200

    place list item style started

--- app/src/main/java/mosis/ivana/mustsee/FriendsUniversalActivity.java
+++ app/src/main/java/mosis/ivana/mustsee/FriendsUniversalActivity.java
@@ -32,7 +32,7 @@ public class FriendsUniversalActivity extends AppCompatActivity implements Adapt
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         ListView friendList= findViewById(R.id.universalFriendsList);
         friendsAdapter= new ListFriendsAdapter(new ArrayList<BasicFriendInfo>(),this);
